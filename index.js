6705a021dc0b9eed6a1a5c26a71bb81878fcfa8b
Ankit377 <westartencare@gmail.com>
2020-09-24 16:27:57 +0530

    input removed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// detect click event
+
 for (i = 0; i < (document.querySelectorAll(".drum").length); i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", drumPlay)
   function drumPlay() {
@@ -6,84 +8,12 @@ for (i = 0; i < (document.querySelectorAll(".drum").length); i++) {
 
     var buttoninnerHtml = this.innerHTML
     console.log(buttoninnerHtml);
-
-    switch (buttoninnerHtml) {
-      case "w":
-        var audio = new Audio("./sounds/crash.mp3");
-        audio.play()
-        break;
-      case "a":
-        var audio = new Audio("./sounds/kick-bass.mp3");
-        audio.play()
-        break;
-      case "s":
-        var audio = new Audio("./sounds/snare.mp3");
-        audio.play()
-        break;
-      case "d":
-        var audio = new Audio("./sounds/tom-1.mp3");
-        audio.play()
-        break;
-      case "j":
-        var audio = new Audio("./sounds/tom-2.mp3");
-        audio.play()
-        break;
-      case "k":
-        var audio = new Audio("./sounds/tom-3.mp3");
-        audio.play()
-        break;
-      case "l":
-        var audio = new Audio("./sounds/tom-4.mp3");
-        audio.play()
-        break;
-
-      default:
-        console.log(buttoninnerHtml);
-
-        break;
-    }
-
-
-
-
-
-
-
-    // if (this.i == 0) {
-    //   var audio = new Audio("./sounds/crash.mp3");
-    //   this.audio.play()
-    // }
-    // else if (this.i == 1) {
-    //   var audio = new Audio("./sounds/tom-4.mp3");
-    //   audio.play()
-    // }
-    // else if (this.i == 2) {
-    //   var audio = new Audio("./sounds/crash.mp3");
-    //   audio.play()
-    // }
-    // else if (this.i == 3) {
-    //   var audio = new Audio("./sounds/crash.mp3");
-    //   audio.play()
-    // }
-    // else if (this.i == 4) {
-    //   var audio = new Audio("./sounds/crash.mp3");
-    //   audio.play()
-    // } else if (this.i == 5) {
-    //   var audio = new Audio("./sounds/crash.mp3");
-    //   audio.play()
-    // }
-
-    // else {
-    //   var audio = new Audio("./sounds/tom-1.mp3");
-    //   alert("dkgj")
-    //   audio.play()
-
-    // }
-
+    makeSound(buttoninnerHtml);
   }
 }
 
-// getting keyboard input
+
+/// keypress detect
 
 addEventListener("keypress", drumkeyplay)
 
@@ -92,8 +22,15 @@ function drumkeyplay(e) {
 
   var pressedkey = e.key
 
+  makeSound(pressedkey)
 
-  switch (pressedkey) {
+}
+
+// getting keyboard input
+
+function makeSound(key) {
+
+  switch (key) {
     case "w":
       var audio = new Audio("./sounds/crash.mp3");
       audio.play()
@@ -128,5 +65,4 @@ function drumkeyplay(e) {
 
       break;
   }
-
 }
